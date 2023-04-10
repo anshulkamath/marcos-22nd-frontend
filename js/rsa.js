0481c261c0a29671ff662e8b7ee3656b3de02ac7
Anshul Kamath <anshulkam@gmail.com>
2023-04-09 20:35:47 -0700

    finished preliminary RSA puzzle

---
+++ js/rsa.js
@@ -0,0 +1,15 @@
+const endpoint = 'http://localhost:8080/rsa'
+
+const makeRequest = async () => {
+  const requestParams = {
+    type: 'GET',
+    contentType: 'application/json',
+  }
+  
+  const response = await $.ajax(endpoint, requestParams)
+  $('#help-link').attr('href', response.link)
+}
+
+jQuery(async () => {
+  await makeRequest()
+})
